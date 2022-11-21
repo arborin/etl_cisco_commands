dfdb172abc310a1e899174407c437c855487ea51
nkobaidze <nika.kobaidze@gmail.com>
2022-11-21 18:01:29 +0400

    [ADD] parser

--- etl.py
+++ etl.py
@@ -1,15 +1,16 @@
 import json
 import logging
 import sys
+import re
 
-from airflow.exceptions import AirflowFailException
+# from airflow.exceptions import AirflowFailException
 
 logger = logging.getLogger(__name__)
 
 def get_parsed_job_output(job_name, raw_data):
     if job_name == 'version':
         pass
-        # return get_parsed_version_job_output(raw_data)
+        return get_parsed_version_job_output(raw_data)
 
     if job_name == 'inventory':
         pass
@@ -18,15 +19,34 @@ def get_parsed_job_output(job_name, raw_data):
 
     logger.info('invalid job_name in config.')
     sys.tracebacklimit = 0 # set sys.tracebacklimit = None to enable it back
-    raise AirflowFailException("invalid job_name in config.") # task fails without retrying
+    # raise AirflowFailException("invalid job_name in config.") # task fails without retrying
+
+
+def get_parsed_version_job_output(raw_data):
+
+    # "IOSv Software": "VIOS-ADVENTERPRISEK9-M",
+    # "Version": "15.9(3)M4",
+    # "RELEASE SOFTWARE": "fc3"
+    read_json_data = False
+    info = ''
+    start = raw_data.index('{')
+    end = raw_data.rindex('}')
+    
+    print(type(raw_data))
+    print(start, end)
+    print(raw_data)
+
 
 def main():
-
-    file_path = 'path to cisco-nxos-show-inventory.txt'
-    job_name = 'inventory'
+    
+    file_path = 'cisco-nxos-show-version.txt'
+    job_name = 'version'
 
     # file_path = 'path to cisco-nxos-show-version.txt'
     # job_name = 'version'
+    
+    # parse argument list
+    
 
 
     with open(file_path, 'r') as f:
