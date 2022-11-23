fb77a327806fa4237f53dde7024bc4e2cf73d1ab
arborin <nika.kobaidze@gmail.com>
2022-11-23 23:11:05 +0400

    [ADD] json parser

--- etl.py
+++ etl.py
@@ -22,10 +22,40 @@ def get_parsed_job_output(job_name, raw_data):
     # raise AirflowFailException("invalid job_name in config.") # task fails without retrying
 
 
-def result_search(pattern, text):
-    result = re.search(pattern, text)
-    if result:
-        return result.group(1)
+# def result_search(pattern, text):
+#     result = re.search(pattern, text)
+#     if result:
+#         return result.group(1)
+
+
+# def get_parsed_version_job_output(raw_data):
+#     # {
+#     #   "IOSv Software": "VIOS-ADVENTERPRISEK9-M",
+#     #   "Version": "15.9(3)M4",
+#     #   "RELEASE SOFTWARE": "fc3"
+#     # }
+#     # error handling
+#     result = {}
+#     result['IOSv Software'] = result_search('Software \\((.+?)\\)', raw_data)
+#     result['Version'] = result_search('Version (.+?),', raw_data)
+#     result['RELEASE SOFTWARE'] = result_search(
+#         'SOFTWARE \\((.+?)\\)', raw_data)
+
+#     print(result)
+
+
+def get_json_from_text(raw_data):
+
+    start = raw_data.find("{")  # start index
+    end = raw_data.rfind("}")  # end index
+    json_str = raw_data[start:end+1]  # get json txt
+
+    clean_str = ''
+
+    for line in json_str.split('\n'):
+        clean_str += line.replace('\\n', ',').strip()
+
+    return json.loads(clean_str)
 
 
 def get_parsed_version_job_output(raw_data):
@@ -35,16 +65,14 @@ def get_parsed_version_job_output(raw_data):
     #   "RELEASE SOFTWARE": "fc3"
     # }
 
-    print("\n-------------------------------------\n")
-    result = {}
-    result['IOSv Software'] = result_search('Software \\((.+?)\\)', raw_data)
+    json_data = {}
+    result = get_json_from_text(raw_data)
+    result = result['msg']['stdout'][0].split(',')
+    json_data['iosv_software'] = result[1].strip()
+    json_data['version'] = result[2].strip()
+    json_data['release_software'] = result[3].strip()
 
-    result['Version'] = result_search('Version (.+?),', raw_data)
-    result['RELEASE SOFTWARE'] = result_search(
-        'SOFTWARE \\((.+?)\\)', raw_data)
-
-    print(result)
-    print("\n-------------------------------------\n")
+    print(json_data)
 
 
 def main():
